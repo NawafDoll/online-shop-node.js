bcb8aa3e2c6cd783821129c9d5b485a2518b1bce
Nawaf <nawaftalaat@hotmail.com>
2023-05-04 14:59:39 +0300

    App

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://online-shop-mbej.onrender.com",
+    origin: "https://online-shop-mbej.onrender.com/",
     methods: ["GET", "POST"],
     credentials: true,
   })
